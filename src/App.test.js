4fe158666c4c423d97208d0f19e66d24731c34b8
Leon Suo <suojunchao@gmail.com>
2020-05-23 09:39:20 -0500

    update structure

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = mount(<App />);
-});
--- src/Container/App.js
+++ src/Container/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -7,11 +9,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
+          data-testid="app_top_label"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
