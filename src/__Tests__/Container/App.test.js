f82414ea49535166ecd6c976bfd4a033329fd0a1
Leon Suo <suojunchao@gmail.com>
2020-05-24 13:12:55 -0500

    update

--- src/__Tests__/Container/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../../Container/App';
-
-test('renders learn react link', () => {
-  const wrapper = shallow(<App />);
-  const label = wrapper.find('[data-testid="app_top_label"]').text();
-  expect(label).toEqual('Learn React');
-});
