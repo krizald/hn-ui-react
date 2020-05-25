7ca2ea3ee47092843d473cd25cb3fa602b3de2dc
Leon Suo <suojunchao@gmail.com>
2020-05-24 21:43:18 -0500

    update

--- src/__tests__/api/hackernewsApi.ts
+++ src/__tests__/api/hackernewsApi.ts
@@ -4,35 +4,64 @@ import { IItem } from '../../model';
 
 jest.mock('axios');
 
-test('pull single item', async () => {
-  const data: IItem = {
-    by: 'farmer',
-    id: 12,
-    deleted: false,
-    kids: [454421],
-    score: 5,
-    time: 1160422112,
-    title: 'Wired: The Desktop is Dead',
-    type: 'story',
-    url: 'http://wired.com/wired/archive/14.10/cloudware.html',
-  };
+describe('Hacker News Api test suites', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-  const mockResponse: AxiosResponse<IItem> = {
-    data,
-    status: 200,
-    statusText: 'OK',
-    config: {},
-    headers: {},
-  };
-  (axios.get as jest.Mock).mockImplementationOnce(() =>
-    Promise.resolve(mockResponse),
-  );
+  test('It should return a single item json', async () => {
+    const data: IItem = {
+      by: 'farmer',
+      id: 12,
+      deleted: false,
+      kids: [454421],
+      score: 5,
+      time: 1160422112,
+      title: 'Wired: The Desktop is Dead',
+      type: 'story',
+      url: 'http://wired.com/wired/archive/14.10/cloudware.html',
+    };
 
-  const api = new HackerNewsApi();
-  const response = await api.fetchItem(12);
-  expect(response.data).toBe(data);
-  expect(axios.get).toHaveBeenCalledTimes(1);
-  expect(axios.get).toHaveBeenCalledWith(
-    'https://hacker-news.firebaseio.com/v0/item/12.json',
-  );
+    const mockResponse: AxiosResponse<IItem> = {
+      data,
+      status: 200,
+      statusText: 'OK',
+      config: {},
+      headers: {},
+    };
+    (axios.get as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve(mockResponse),
+    );
+
+    const api = new HackerNewsApi();
+    const response = await api.fetchItem(12);
+    expect(response.data).toBe(data);
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(
+      'https://hacker-news.firebaseio.com/v0/item/12.json',
+    );
+  });
+
+  test('It should return a list of ids', async () => {
+    const data = [1, 2, 3, 4, 5, 6, 10];
+
+    const mockResponse: AxiosResponse<Array<number>> = {
+      data,
+      status: 200,
+      statusText: 'OK',
+      config: {},
+      headers: {},
+    };
+    (axios.get as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve(mockResponse),
+    );
+
+    const api = new HackerNewsApi();
+    const response = await api.fetchTopStories();
+    expect(response.data).toBe(data);
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(
+      'https://hacker-news.firebaseio.com/v0/topstories.json',
+    );
+  });
 });
--- src/api/hackerNewsApi.ts
+++ src/api/hackerNewsApi.ts
@@ -3,8 +3,11 @@ import apiConfig from './apiConfig';
 import { IItem } from '../model';
 
 export default class HackerNewsApi {
-  fetchItem = (id: number): Promise<AxiosResponse<IItem>> => {
-    const query = `${apiConfig.databaseURL + apiConfig.version}/item/${id}.json`;
-    return axios.get<IItem>(query);
-  };
+  fetchItem = (id: number): Promise<AxiosResponse<IItem>> =>
+    axios.get<IItem>(`${apiConfig.databaseURL + apiConfig.version}/item/${id}.json`);
+
+  fetchTopStories = (): Promise<AxiosResponse<Array<number>>> =>
+    axios.get<Array<number>>(
+      `${apiConfig.databaseURL + apiConfig.version}/topstories.json`,
+    );
 }
