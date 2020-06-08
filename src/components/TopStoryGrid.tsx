1a823de574133a346cfa8781e5e7280cd4834357
Leon Suo <suojunchao@gmail.com>
2020-06-08 10:45:11 -0500

    issue #1: update

--- src/components/TopStoryGrid.tsx
+++ src/components/TopStoryGrid.tsx
@@ -1,13 +1,38 @@
 import React, { FC, useState, useEffect } from 'react';
-import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import { withStyles, WithStyles } from '@material-ui/core/styles';
+import { GridReadyEvent, ValueFormatterParams } from 'ag-grid-community';
+import moment from 'moment';
 import { StoryStore } from '../stores';
 import { ItemModel } from '../model';
+import LinkCellRenderer from './LinkCellRenderer';
 
 const styles = { gridcontainer: { height: '800px', width: '100%' } };
 type TopStoryGriProps = WithStyles<typeof styles>;
+const gridOptions = {
+  columnDefs: [
+    { field: 'title', headerName: 'Title' },
+    {
+      field: 'url',
+      headerName: 'Link Url',
+      cellRenderer: 'linkCellRenderer',
+    },
+    {
+      field: 'createDate',
+      headerName: 'Date',
+      valueFormatter: (e: ValueFormatterParams): string => {
+        return moment(e.value as Date).format('MMM Do YYYY, hh:mm:ss');
+      },
+    },
+  ],
+  frameworkComponents: {
+    linkCellRenderer: LinkCellRenderer,
+  },
+  pagination: true,
+  paginationAutoPageSize: true,
+};
 
 const UnstyledTopStoryGrid: FC<TopStoryGriProps> = (props: TopStoryGriProps) => {
   const { classes } = props;
@@ -39,13 +64,13 @@ const UnstyledTopStoryGrid: FC<TopStoryGriProps> = (props: TopStoryGriProps) => 
       >
         Clean
       </button>
-      <AgGridReact rowData={stories}>
-        <AgGridColumn field="id" />
-        <AgGridColumn field="type" />
-        <AgGridColumn field="title" />
-        <AgGridColumn field="url" />
-        <AgGridColumn field="createDate" />
-      </AgGridReact>
+      <AgGridReact
+        rowData={stories}
+        gridOptions={gridOptions}
+        onGridReady={(e: GridReadyEvent): void => {
+          e.api.sizeColumnsToFit();
+        }}
+      />
     </div>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './TopStoryGrid';
 export { default as TopStoryGrid } from './TopStoryGrid';
+export { default as LinkCellRenderer } from './LinkCellRenderer';
+export * from './LinkCellRenderer';
